ffb8d795db99e43387d8df459b7a6d2e3eb34b3a
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-02-16 14:39:36 -0300

    FIC-W01: WIP

--- lib/card_colors.dart
+++ lib/card_colors.dart
@@ -4,13 +4,13 @@ class CardColors {
   static final CardColors _instance = CardColors._internal();
 
   // Primary
-  final softBlue = const Color(0xff0000ff);
-  final cyan = const Color(0xff0000aa);
+  final softBlue = const Color(0xff8BACDA);
+  final cyan = const Color(0xff00FFF7);
 
   // Neutral
-  final background = const Color(0xff22ff22);
-  final cardBackground = const Color(0xff2bbfff);
-  final line = const Color(0xff2bb5ff);
+  final background = const Color(0xff14253D);
+  final cardBackground = const Color(0xff1B335A);
+  final line = const Color(0xff2F415B);
   final white = const Color(0xffffffff);
 
   factory CardColors() {
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:fic_joaquin_leimeter/card_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
 
 void main() {
   CardColors();
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: CardColors().background,
         body: const SafeArea(
           child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 100, horizontal: 40),
+            padding: EdgeInsets.fromLTRB(40, 80, 40, 0),
             child: MyCard(),
           ),
         ),
@@ -37,26 +38,116 @@ class MyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: const BoxDecoration(color: Colors.amber),
+    return Container(
+      padding: const EdgeInsets.all(25),
+      decoration: BoxDecoration(
+        color: CardColors().cardBackground,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(16),
+        ),
+      ),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Text(
-            'AAA',
-            style: TextStyle(fontWeight: FontWeight.w600),
-          ),
-          const Text('asd'),
-          const Text('asd'),
-          Align(
-            alignment: Alignment.topLeft,
-            child: SvgPicture.asset(
-              'assets/icon-ethereum.svg',
-              semanticsLabel: 'Acme Logo',
+          // image
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: const Image(
+              image: AssetImage('assets/card/images/image-equilibrium.jpg'),
             ),
-          )
+          ),
+
+          const Gap(20),
+
+          // Title
+          Text(
+            'Title',
+            style: TextStyle(color: CardColors().white),
+          ),
+
+          const Gap(20),
+
+          // description
+          const Text('Description'),
+
+          const Gap(20),
+
+          // price & expiration
+          Row(
+            children: [
+              Row(
+                children: [
+                  // Etherium Icon
+                  SvgPicture.asset(
+                    'assets/card/icons/icon-ethereum.svg',
+                    semanticsLabel: 'Acme Logo',
+                  ),
+
+                  // price with ETH, maybe wrap in Row
+                  const Text('0.041'),
+                  const Text('ETH'),
+                ],
+              ),
+              Row(
+                children: [
+                  // Clok icon
+                  SvgPicture.asset(
+                    'assets/card/icons/icon-ethereum.svg',
+                    semanticsLabel: 'Acme Logo',
+                  ),
+
+                  // days left
+                  const Text('3 days left')
+                ],
+              )
+            ],
+          ),
+
+          const Gap(20),
+
+          // Divider
+          Divider(color: CardColors().white),
+
+          const Gap(20),
+
+          // user
+          Row(
+            children: const [
+              // User Icon
+              Image(
+                image: AssetImage('assets/card/images/image-avatar.png'),
+              ),
+
+              // Creation of Jules Wyvern
+              Text('creation of this dude')
+            ],
+          ),
         ],
       ),
     );
   }
 }
+
+
+          //   'AAA',
+          //   style: TextStyle(fontWeight: FontWeight.w600),
+          // ),
+          // const Text('asd'),
+          // const Text('asd'),
+          // Align(
+          //   alignment: Alignment.topLeft,
+          //   child: SvgPicture.asset(
+          //     'assets/icon-ethereum.svg',
+          //     semanticsLabel: 'Acme Logo',
+          //   ),
+          // )
+
+          // Align(
+          //   alignment: Alignment.topLeft,
+          //   child: SvgPicture.asset(
+          //     'assets/card/icons/icon-ethereum.svg',
+          //     semanticsLabel: 'Acme Logo',
+          //   ),
+          // ),
+
