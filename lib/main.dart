59a1b640a21af54dedee39e9781dbb6fbdd580a6
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-02-19 12:51:39 -0300

    FIC-W01: remove card challenge from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:google_fonts/google_fonts.dart';
-import 'package:fic_joaquin_leimeter/card_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  CardColors();
   runApp(const MyApp());
 }
 
@@ -17,45 +14,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        textTheme: GoogleFonts.outfitTextTheme(),
+        textTheme: GoogleFonts.robotoTextTheme(),
       ),
       home: Scaffold(
-        backgroundColor: CardColors().background,
-        body: const SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 100, horizontal: 40),
-            child: MyCard(),
-          ),
+        appBar: AppBar(title: const Text('Flutter full course')),
+        body: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Placeholder(),
         ),
-      ),
-    );
-  }
-}
-
-class MyCard extends StatelessWidget {
-  const MyCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: const BoxDecoration(color: Colors.amber),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Text(
-            'AAA',
-            style: TextStyle(fontWeight: FontWeight.w600),
-          ),
-          const Text('asd'),
-          const Text('asd'),
-          Align(
-            alignment: Alignment.topLeft,
-            child: SvgPicture.asset(
-              'assets/icon-ethereum.svg',
-              semanticsLabel: 'Acme Logo',
-            ),
-          )
-        ],
       ),
     );
   }
