5028ea7af00ad4b8789bafafeafa4d864a9bae14
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-02-22 13:22:05 -0300

    add firebase - add task_model file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fic_joaquin_leimeter/models/task_model.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _firebaseApp = Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // This widget is the root of your application.
   @override
@@ -18,11 +27,73 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter full course')),
-        body: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Placeholder(),
+        body: FutureBuilder(
+          future: _firebaseApp,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const MyList();
+            }
+            if (snapshot.hasError) {
+              return Text('error: ${snapshot.error}');
+            }
+            return const Text('loading');
+          },
         ),
       ),
+    );
+  }
+}
+
+class MyList extends StatefulWidget {
+  const MyList({super.key});
+
+  @override
+  State<MyList> createState() => _MyListState();
+}
+
+class _MyListState extends State<MyList> {
+  Future<List<TaskModel>> _getTasks() async {
+    List<TaskModel> tasks = [];
+
+    CollectionReference collectionRef =
+        FirebaseFirestore.instance.collection('tasks');
+    QuerySnapshot tasksQuerySnapshot = await collectionRef.get();
+    List<QueryDocumentSnapshot> tasksDoc = tasksQuerySnapshot.docs;
+
+    for (QueryDocumentSnapshot doc in tasksDoc) {
+      var task = doc.data();
+      print('task: $task');
+      tasks.add(TaskModel.fromJson(doc.data() as Map<String, dynamic>));
+    }
+
+    return tasks;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _getTasks(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              TaskModel task = snapshot.data![index];
+              return Column(
+                children: [
+                  Text(task.title),
+                  Text(task.description),
+                  Text('${task.done}'),
+                ],
+              );
+            },
+          );
+        }
+        if (snapshot.hasError) {
+          return Text('error: ${snapshot.error}');
+        }
+        return const Text('loading');
+      },
     );
   }
 }
---
+++ lib/models/task_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TaskModel _$TaskModelFromJson(Map<String, dynamic> json) => TaskModel(
+      title: json['title'] as String,
+      description: json['description'] as String,
+      done: json['done'] as bool?,
+    );
+
+Map<String, dynamic> _$TaskModelToJson(TaskModel instance) => <String, dynamic>{
+      'title': instance.title,
+      'description': instance.description,
+      'done': instance.done,
+    };
